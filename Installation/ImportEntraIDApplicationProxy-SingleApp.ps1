1d83b46a912457a4db44c0c609a2c0ee5bde6332
Mathieu Simon <mathieu.simon@iseag.ch>
2026-08-25 16:39:54 +0200

    ImportEntraIDApplicationProxy-SingleApp.ps1: Change names and parameters
    Switched to ClientId and ClientSecret references, start replacing
    authentication to not depend on the ClientId of Microsoft Azure PowerShell.

--- Installation/ImportEntraIDApplicationProxy-SingleApp.ps1
+++ Installation/ImportEntraIDApplicationProxy-SingleApp.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-Imports a cert from simple-acme renewal into Azure AD Application Proxy for all applications that are using it. You likely want to use a wildcard certificate for this purpose.
+Imports a cert from simple-acme renewal into Entra ID application proxy for a specific application that is using it. This way wildcard certificates are not required.
 
 .DESCRIPTION
 Note that this script is intended to be run via the install script plugin from simple-acme via the batch script wrapper. As such, we use positional parameters to avoid issues with using a dash in the cmd line. 
@@ -8,19 +8,30 @@ Note that this script is intended to be run via the install script plugin from s
 Uses Microsoft Graph API instead of deprecated AzureAD module.
 
 .PARAMETER PfxPath
-The absolute path to the pfx file that will be uploaded to Azure. Typically use '{CacheFile}'
+The absolute path to the pfx file that will be uploaded to Entra ID. Typically use '{CacheFile}'
 
 .PARAMETER CertPass
 The password for the pfx file. Typically use '{CachePassword}'
 
-.PARAMETER Username
-Username of account to login with Connect-MgGraph. This account must have the "Application administrator" role.
+.PARAMETER ClientId
+The application (client) ID of the app registration used to authenticate.
 
-.PARAMETER Password
-Password for the account
+There are 2 suggested ways to do this:
+1. Generate a secret for each proxied application and pass the client ID 
+   of each app registration.
+2. If you have several proxied applications, and want to avoid having many
+   individual secrets: 
+   - Create an app registration in your tenant solely used to manage the certificates 
+     of proxied applications.
+   - Create a secret
+   - Grant 'Application.ReadWrite.OwnedBy' as Application to this app registration.
+   - Add this app registration as owner of each individual proxied application.
+
+.PARAMETER ClientSecret
+Secret used to connect with your ClientId.
 
 .EXAMPLE 
-ImportAzureApplicationGateway.ps1 <PfxPath> <CertPass> <Username> <Password>
+ImportEntraIDApplicationProxy-SingleApp.ps1 <PfxPath> <CertPass> <ClientId> <ClientSecret>
 
 .NOTES
 This uses the Microsoft Graph API instead of the deprecated Azure AD module which no longer works.
@@ -30,35 +41,18 @@ Unfortunately, the graph API doesn't have good (or any really) documentation abo
 param(
     [Parameter(Position=0,Mandatory=$true)][string]$PfxPath,
     [Parameter(Position=1,Mandatory=$true)][string]$CertPass,
-    [Parameter(Position=2,Mandatory=$true)][string]$Username,
-    [Parameter(Position=3,Mandatory=$true)][string]$Password
+    [Parameter(Position=2,Mandatory=$true)][string]$ClientId,
+    [Parameter(Position=3,Mandatory=$true)][string]$ClientSecret
 )
 
 if (!(Get-Command "Get-MGBetaApplication" -ErrorAction SilentlyContinue)) {
     Throw "Missing Microsoft.Graph.Beta module, install with 'Install-Module -Name Microsoft.Graph.Beta -Scope AllUsers'"
 } 
 
-# Connect to Microsoft Graph using username/password
-$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
-$ClientId = "1950a258-227b-4e31-a9cf-717495945fc2" # Microsoft Azure PowerShell app ID
-
-# Get access token
-$body = @{
-    grant_type = "password"
-    client_id = $ClientId
-    username = $Username
-    password = $Password
-    scope = "https://graph.microsoft.com/.default"
-}
-
-$tokenResponse = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/organizations/oauth2/v2.0/token" -Body $body
-$accessToken = $tokenResponse.access_token | ConvertTo-SecureString -AsPlainText -Force
-
-Connect-MgGraph -AccessToken $accessToken -NoWelcome
-
-
-
-
+# Connect to Microsoft Graph using ClientId/ClientSecret
+$SecureSecret = ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force
+$ClientSecretCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ClientId, $SecureSecret
+$null = Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $ClientSecretCredential -NoWelcome
 
 # Get service principals tagged with WindowsAzureActiveDirectoryOnPremApp
 $aadapServPrinc = Get-MgBetaServicePrincipal -All | Where-Object {$_.Tags -Contains "WindowsAzureActiveDirectoryOnPremApp"}
